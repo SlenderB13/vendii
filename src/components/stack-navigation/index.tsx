27cadab14dad584e44c2d874cfc694792da668d0
Lucas Silva <lguilherme8@gmail.com>
2023-10-19 11:57:48 -0300

    refact: stack navigation to component

---
+++ src/components/stack-navigation/index.tsx
@@ -0,0 +1,17 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import Campaigns from "../../screens/campaigns";
+import AddCostumer from "../../screens/add-costumer";
+
+export default function StackNavigation() {
+  const Stack = createStackNavigator();
+
+  return (
+    <Stack.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName="Campaigns"
+    >
+      <Stack.Screen name="Campaigns" component={Campaigns} />
+      <Stack.Screen name="Add Costumer" component={AddCostumer} />
+    </Stack.Navigator>
+  );
+}
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,21 +1,11 @@
 import { SafeAreaView } from "react-native";
 import tw from "twrnc";
-import AddCostumer from "../add-costumer";
-import { createStackNavigator } from "@react-navigation/stack";
-import Campaigns from "../campaigns";
+import StackNavigation from "../../components/stack-navigation";
 
 export default function Home() {
-  const Stack = createStackNavigator();
-
   return (
     <SafeAreaView style={tw`h-full w-full`}>
-      <Stack.Navigator
-        screenOptions={{ headerShown: false }}
-        initialRouteName="Campaigns"
-      >
-        <Stack.Screen name="Campaigns" component={Campaigns} />
-        <Stack.Screen name="Add Costumer" component={AddCostumer} />
-      </Stack.Navigator>
+      <StackNavigation />
     </SafeAreaView>
   );
 }
