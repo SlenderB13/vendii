5f8994d357592be76b99fc974931297b39a9816b
Lucas Silva <lguilherme8@gmail.com>
2023-11-03 19:47:40 -0300

    add: text-input component

---
+++ src/components/text-input/index.tsx
@@ -0,0 +1,25 @@
+import { TextInput } from "react-native-paper";
+import { IconSource } from "react-native-paper/lib/typescript/components/Icon";
+import tw from "twrnc";
+
+interface inputProps {
+  label: string;
+  icon?: IconSource;
+}
+
+export default function Input({ label, icon }: inputProps) {
+  return (
+    <TextInput
+      label={label}
+      mode="outlined"
+      selectionColor="black"
+      outlineColor="black"
+      activeOutlineColor="black"
+      style={tw`bg-white`}
+      outlineStyle={{
+        borderRadius: 10,
+      }}
+      left={icon && <TextInput.Icon icon={icon} />}
+    />
+  );
+}
