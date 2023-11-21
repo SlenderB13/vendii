bd8849f0939494ad0a3f16b27b1a2b75f2a001a7
Lucas Silva <lguilherme8@gmail.com>
2023-11-21 16:59:01 -0300

    add: rendering costumers in list

--- src/screens/costumers/index.tsx
+++ src/screens/costumers/index.tsx
@@ -1,14 +1,28 @@
-import { SafeAreaView, View } from "react-native";
+import { FlatList, SafeAreaView, View } from "react-native";
 import { Text } from "react-native-paper";
 import AppHeader from "../../components/app-header";
 import Header from "../../components/header";
+import { storage } from "../../lib/react-native-mmkv";
+import { Costumer } from "../../types/costumer.interface";
+import { useEffect } from "react";
 
 export default function Costumers() {
+  const costumersJson: string | undefined = storage.getString("costumers");
+  let costumers: Costumer[] = [];
+
+  if (costumersJson) {
+    costumers = JSON.parse(costumersJson);
+  }
+
   return (
     <SafeAreaView>
       <AppHeader />
       <View>
         <Header title="Meus clientes" />
+        <FlatList
+          data={costumers}
+          renderItem={(item) => <Text>{item.item.name}</Text>}
+        />
       </View>
     </SafeAreaView>
   );
