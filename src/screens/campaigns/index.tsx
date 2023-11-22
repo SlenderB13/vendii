028dcaebc94d6d23b109edc9f02949f312a82417
Lucas Silva <lguilherme8@gmail.com>
2023-11-22 17:31:29 -0300

    edit: FAB button individually in costumers and home page

--- src/screens/campaigns/index.tsx
+++ src/screens/campaigns/index.tsx
@@ -1,13 +1,25 @@
 import { SafeAreaView, View } from "react-native";
-import { Text } from "react-native-paper";
+import { FAB, Portal, Text } from "react-native-paper";
 import Header from "../../components/header";
+import { useNavigation } from "@react-navigation/native";
+import tw from "twrnc";
 
 export default function Campaigns() {
+  const navigation = useNavigation();
   return (
     <SafeAreaView>
       <View>
         <Header title="Início" />
       </View>
+      <FAB
+        style={tw`absolute right-4 top-123 bg-zinc-900`}
+        icon="plus"
+        label="Adicionar cliente"
+        color="white"
+        onPress={() => {
+          navigation.navigate("Add Costumer");
+        }}
+      />
     </SafeAreaView>
   );
 }
--- src/screens/costumers/index.tsx
+++ src/screens/costumers/index.tsx
@@ -5,10 +5,13 @@ import { storage } from "../../lib/react-native-mmkv";
 import { Costumer } from "../../types/costumer.interface";
 import { CostumerItem } from "../../components/costumer-item";
 import tw from "twrnc";
+import { FAB, Portal } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Costumers() {
   const costumersJson: string | undefined = storage.getString("costumers");
   let costumers: Costumer[] = [];
+  const navigation = useNavigation();
 
   if (costumersJson) {
     costumers = JSON.parse(costumersJson);
@@ -25,6 +28,15 @@ export default function Costumers() {
           renderItem={(costumers) => <CostumerItem costumer={costumers.item} />}
         />
       </View>
+      <FAB
+        style={tw`absolute right-4 bottom-36 bg-zinc-900`}
+        icon="plus"
+        label="Adicionar cliente"
+        color="white"
+        onPress={() => {
+          navigation.navigate("Add Costumer");
+        }}
+      />
     </SafeAreaView>
   );
 }
