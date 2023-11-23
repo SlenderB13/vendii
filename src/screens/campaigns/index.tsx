9860439b904ba4239f0638bd9bab8ad377664bd2
Lucas Silva <lguilherme8@gmail.com>
2023-11-23 15:26:55 -0300

    add: amount of costumers in home page with svg

--- src/screens/campaigns/index.tsx
+++ src/screens/campaigns/index.tsx
@@ -1,15 +1,38 @@
 import { SafeAreaView, View } from "react-native";
-import { FAB, Portal, Text } from "react-native-paper";
+import { Card, FAB, Portal, Text } from "react-native-paper";
 import Header from "../../components/header";
 import { useNavigation } from "@react-navigation/native";
 import tw from "twrnc";
+import { storage } from "../../lib/react-native-mmkv";
+import { Costumer } from "../../types/costumer.interface";
 
 export default function Campaigns() {
   const navigation = useNavigation();
+
+  const costumersJson: string | undefined = storage.getString("costumers");
+  let costumers: Costumer[] = [];
+  let costumersAmount: number = 0;
+
+  if (costumersJson) {
+    costumers = JSON.parse(costumersJson);
+    costumersAmount = costumers.length;
+  }
+
   return (
     <SafeAreaView>
       <View>
         <Header title="Início" />
+      </View>
+      <View style={tw`p-4`}>
+        <Card>
+          <Card.Cover
+            source={require("../../../assets/undraw-investment.png")}
+          />
+          <Card.Title title="Total de clientes:" />
+          <Card.Content>
+            <Text style={tw`font-bold text-lg`}>{costumersAmount}</Text>
+          </Card.Content>
+        </Card>
       </View>
       <FAB
         style={tw`absolute right-4 top-123 bg-zinc-900`}
